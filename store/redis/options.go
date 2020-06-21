707d8aff805c66237a753cb04350ff31766c6b4b
Hank Shen <swhbox@foxmail.com>
2020-06-21 15:53:17 +0800

    improved

--- store/redis/options.go
+++ store/redis/options.go
@@ -6,112 +6,123 @@ import (
 	"github.com/go-redis/redis"
 )
 
-type RedisOptionsSetter func(*redis.Option)
+type Options struct {
+	*redis.Options
+	KeyNamespace string
+}
+
+type RedisOptionsSetter func(*Options)
 
 func RedisAddr(addr string) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.Addr = addr
 	}
 }
 
 func RedisDB(db int) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.DB = db
 	}
 }
 
 func RedisPassword(password string) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.Password = password
 	}
 }
 
 func RedisUsername(user string) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.Username = user
 	}
 }
 
 func RedisNetwork(network string) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.Network = network
 	}
 }
 
 func RedisMaxRetries(maxRetries int) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.MaxRetries = maxRetries
 	}
 }
 
 func RedisPoolSize(poolSize int) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.PoolSize = poolSize
 	}
 }
 
 func RedisMinIdleConns(minIdleConns int) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.MinIdleConns = minIdleConns
 	}
 }
 
 func RedisMinRetryBackoff(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.MinRetryBackoff = duration
 	}
 }
 
 func RedisMaxRetryBackoff(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.MaxRetryBackoff = duration
 	}
 }
 
 func RedisDialTimeout(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.DialTimeout = duration
 	}
 }
 
 func RedisReadTimeout(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.DialTimeout = duration
 	}
 }
 
 func RedisWriteTimeout(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.WriteTimeout = duration
 	}
 }
 
 func RedisMaxConnAge(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.MaxConnAge = duration
 	}
 }
 
 func RedisPoolTimeout(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.PoolTimeout = duration
 	}
 }
 
 func RedisIdleTimeout(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.IdleTimeout = duration
 	}
 }
 
 func RedisIdleCheckFrequency(duration time.Duration) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.IdleCheckFrequency = duration
 	}
 }
 
 func RedisLimiter(limiter redis.Limiter) RedisOptionsSetter {
-	return func(opt *redis.Option) {
+	return func(opt *Options) {
 		opt.Limiter = limiter
+	}
+}
+
+func RedisKeyNamespace(keyNamespace string) RedisOptionsSetter {
+	return func(opt *Options) {
+		opt.KeyNamespace = keyNamespace
 	}
 }
--- store/redis/store.go
+++ store/redis/store.go
@@ -7,9 +7,19 @@ import (
 )
 
 func New(redisOptions ...RedisOptionsSetter) oauth2.TokenStore {
-	redisConfig := &redis.Options{}
+	redisConfig := &Options{
+		Options: &redis.Options{},
+	}
 	for _, fn := range redisOptions {
 		fn(redisConfig)
 	}
-	return oredis.NewRedisStore(redisConfig)
+	var keyNamespaces []string
+	if len(redisConfig.KeyNamespace) > 0 {
+		keyNamespaces = append(keyNamespaces, redisConfig.KeyNamespace)
+	}
+	return oredis.NewRedisStore(redisConfig.Options, keyNamespaces...)
+}
+
+func NewWithClient(client *redis.Client, keyNamespaces ...string) oauth2.TokenStore {
+	return oredis.NewRedisStoreWithCli(client, keyNamespaces...)
 }
